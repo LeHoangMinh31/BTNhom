20d1b4ab94e1ebaf8c2fc2d9470a02565a770ed3
LeHoangMinh31 <78464622+LeHoangMinh31@users.noreply.github.com>
2022-11-23 18:40:35 +0700

    Add files via upload

---
+++ App_Ban_Giay/components/database/Database.js
@@ -0,0 +1,292 @@
+export const COLOURS = {
+  white: '#ffffff',
+  black: '#000000',
+  green: '#00AC76',
+  red: '#C04345',
+  blue: '#0043F9',
+  backgroundLight: '#F0F0F3',
+  backgroundMedium: '#B9B9B9',
+  backgroundDark: '#777777',
+
+
+  // white: '#d1d1f0',
+  // black: '#000000',
+  // green: '#00AC76',
+  // red: '#C04345',
+  // blue: '#0043F9',
+  // backgroundLight: '#F0F0F3',
+  // backgroundMedium: '#272638',
+  // backgroundDark: '#777777',
+};
+
+export const Items = [
+
+
+
+
+  //giày nike
+  {
+    id: 1,
+    category: 'product',
+    productName: 'Nike_Men_Air_Monarch_IV_Cross_Trainer',
+    productPrice: 147.71,
+    description:
+      'NIKE SHOES MEN: The Nike Air Monarch IV (4E) Training Shoe for Men sets you up for a comfortable training session with durable leather on top for support.',
+    isOff: true,
+    productImage: require('../database/images/products/Nike/Nike_Men_Air_Monarch_IV_Cross_Trainer/1.png'),
+    isAvailable: true,
+    productImageList: [
+      require('../database/images/products/Nike/Nike_Men_Air_Monarch_IV_Cross_Trainer/1.png'),
+      require('../database/images/products/Nike/Nike_Men_Air_Monarch_IV_Cross_Trainer/2.png'),
+      require('../database/images/products/Nike/Nike_Men_Air_Monarch_IV_Cross_Trainer/3.png'),
+    ],
+  },
+
+  {
+    id: 2,
+    category: 'product',
+    productName: 'Nike_Women_Competition_Running_Shoes',
+    productPrice: 306.04,
+    description:
+      'Nike WMNS Nike Air Zoom Vomero 13 922909-006 Women is Running Shoes',
+    isOff: true,
+    productImage: require('../database/images/products/Nike/Nike_Women_Competition_Running_Shoes/1.png'),
+    isAvailable: true,
+    productImageList: [
+      require('../database/images/products/Nike/Nike_Women_Competition_Running_Shoes/1.png'),
+      require('../database/images/products/Nike/Nike_Women_Competition_Running_Shoes/2.png'),
+      require('../database/images/products/Nike/Nike_Women_Competition_Running_Shoes/3.png'),
+    ],
+  },
+  {
+    id: 3,
+    category: 'product',
+    productName: 'Nike_Women_Fitness_Shoes',
+    productPrice: 170.51,
+    description:
+      'The Nike in-season training shoe for women is lightweight, sturdy and has a deep, comfortable footbed for premium impact protection and energy return throughout the workout.',
+    isOff: true,
+    productImage: require('../database/images/products/Nike/Nike_Women_Fitness_Shoes/1.png'),
+    isAvailable: true,
+    productImageList: [
+      require('../database/images/products/Nike/Nike_Women_Fitness_Shoes/1.png'),
+      require('../database/images/products/Nike/Nike_Women_Fitness_Shoes/2.png'),
+      require('../database/images/products/Nike/Nike_Women_Fitness_Shoes/3.png'),
+    ],
+  },
+  {
+    id: 4,
+    category: 'product',
+    productName: 'Nike_Women_Sneaker_Running_Shoes',
+    productPrice: 292.71,
+    description:
+      'Womens running shoes from Nike. Model - WMNS Nike FREE RN 2018 Reference 942837-007',
+    isOff: true,
+    productImage: require('../database/images/products/Nike/Nike_Women_Sneaker_Running_Shoes/1.png'),
+    isAvailable: true,
+    productImageList: [
+      require('../database/images/products/Nike/Nike_Women_Sneaker_Running_Shoes/1.png'),
+      require('../database/images/products/Nike/Nike_Women_Sneaker_Running_Shoes/2.png'),
+      require('../database/images/products/Nike/Nike_Women_Sneaker_Running_Shoes/3.png'),
+    ],
+  },
+  {
+    id: 5,
+    category: 'product',
+    productName: 'Nike_Womens_Free_Metcon_3_Training_Shoe_Cj6314-068_Size_10',
+    productPrice: 292.71,
+    description:
+      'Womens running shoes from Nike. Model - WMNS Nike FREE RN 2018 Reference 942837-007',
+    isOff: true,
+    productImage: require('../database/images/products/Nike/Nike_Womens_Free_Metcon_3_Training_Shoe_Cj6314-068_Size_10/1.png'),
+    isAvailable: true,
+    productImageList: [
+      require('../database/images/products/Nike/Nike_Womens_Free_Metcon_3_Training_Shoe_Cj6314-068_Size_10/1.png'),
+      require('../database/images/products/Nike/Nike_Womens_Free_Metcon_3_Training_Shoe_Cj6314-068_Size_10/2.png'),
+      require('../database/images/products/Nike/Nike_Womens_Free_Metcon_3_Training_Shoe_Cj6314-068_Size_10/3.png'),
+    ],
+  },
+
+  {
+    id: 6,
+    category: 'product',
+    productName: 'Nike_Womens_Metcon_3 Training_Shoes_BlackWhite_Size_7',
+    productPrice: 397,
+    description:
+      'DURABLE AND STABLE. The fully updated Nike Metcon 3 Women Training Shoe is ready for your most demanding workouts—from wall exercises and rope climbs to sprinting and lifting.',
+    isOff: true,
+    productImage: require('../database/images/products/Nike/Nike_Womens_Metcon_3Training_Shoes_BlackWhite_Size_7/1.png'),
+    isAvailable: true,
+    productImageList: [
+      require('../database/images/products/Nike/Nike_Womens_Metcon_3Training_Shoes_BlackWhite_Size_7/1.png'),
+      require('../database/images/products/Nike/Nike_Womens_Metcon_3Training_Shoes_BlackWhite_Size_7/2.png'),
+      require('../database/images/products/Nike/Nike_Womens_Metcon_3Training_Shoes_BlackWhite_Size_7/3.png'),
+    ],
+  },
+  //giày vans
+  {
+    id: 7,
+    category: 'product',
+    productName: 'Vans_Men_Low-Top_Sneakers',
+    productPrice: 89.40,
+    description:
+      'Inspired by Vans Classic Old Skool, the Ward is a low top, vulcanized lace up style with Vans signature side stripe, a round toe, padded collar for extra comfort, a smooth lining and a cushioning insole.',
+    isOff: true,
+    productImage: require('../database/images/products/Vans/Vans_Men_Low-Top_Sneakers/1.png'),
+    isAvailable: true,
+    productImageList: [
+      require('../database/images/products/Vans/Vans_Men_Low-Top_Sneakers/1.png'),
+      require('../database/images/products/Vans/Vans_Men_Low-Top_Sneakers/2.png'),
+      require('../database/images/products/Vans/Vans_Men_Low-Top_Sneakers/3.png'),
+    ],
+  },
+  {
+    id: 8,
+    category: 'product',
+    productName: 'Vans_Men_Old_Skool_Sneaker',
+    productPrice: 115.00,
+    description:
+      'DURABLE AND STABLE. The fully updated Nike Metcon 3 Women Training Shoe is ready for your most demanding workouts—from wall exercises and rope climbs to sprinting and lifting.',
+    isOff: true,
+    productImage: require('../database/images/products/Vans/Vans_Men_Old_Skool_Sneaker/1.png'),
+    isAvailable: true,
+    productImageList: [
+      require('../database/images/products/Vans/Vans_Men_Old_Skool_Sneaker/1.png'),
+      require('../database/images/products/Vans/Vans_Men_Old_Skool_Sneaker/2.png'),
+      require('../database/images/products/Vans/Vans_Men_Old_Skool_Sneaker/3.png'),
+    ],
+  },
+  {
+    id: 9,
+    category: 'product',
+    productName: 'Vans_Unisex_Slip-on(tm)_Core_Classics_Low-Top_Trainers',
+    productPrice: 129.00,
+    description:
+      'The shoe that started it all. The iconic Vans Classic Slip-On keeps it simple and classic.',
+    isOff: true,
+    productImage: require('../database/images/products/Vans/Vans_Unisex_Slip-on(tm)_Core_Classics_Low-Top_Trainers/1.png'),
+    isAvailable: true,
+    productImageList: [
+      require('../database/images/products/Vans/Vans_Unisex_Slip-on(tm)_Core_Classics_Low-Top_Trainers/1.png'),
+      require('../database/images/products/Vans/Vans_Unisex_Slip-on(tm)_Core_Classics_Low-Top_Trainers/2.png'),
+      require('../database/images/products/Vans/Vans_Unisex_Slip-on(tm)_Core_Classics_Low-Top_Trainers/3.png'),
+    ],
+  },
+  {
+    id: 10,
+    category: 'product',
+    productName: 'Vans_Unisex-Adult_Sk8-hi_Core_Classics',
+    productPrice: 128.98,
+    description:
+      'This shoe is so rad that it was brought back to life from the past so that you can wear it yourself. Consider that a sign...',
+    isOff: false,
+    productImage: require('../database/images/products/Vans/Vans_Unisex-Adult_Sk8-hi_Core_Classics/1.png'),
+    isAvailable: true,
+    productImageList: [
+      require('../database/images/products/Vans/Vans_Unisex-Adult_Sk8-hi_Core_Classics/1.png'),
+      require('../database/images/products/Vans/Vans_Unisex-Adult_Sk8-hi_Core_Classics/2.png'),
+      require('../database/images/products/Vans/Vans_Unisex-Adult_Sk8-hi_Core_Classics/3.png'),
+    ],
+  },
+  {
+    id: 11,
+    category: 'product',
+    productName: 'Vans_Women_Low-top_Trainers_Sneaker',
+    productPrice: 109.99,
+    description:
+      'Inspired by Vans Classic Old Skool, the Ward is a low top, vulcanized lace up style with Vans signature side stripe, a round toe, padded collar for extra comfort, a smooth lining and a cushioning insole.',
+    isOff: false,
+    productImage: require('../database/images/products/Vans/Vans_Women_Low-top_Trainers_Sneaker/1.png'),
+    isAvailable: true,
+    productImageList: [
+      require('../database/images/products/Vans/Vans_Women_Low-top_Trainers_Sneaker/1.png'),
+      require('../database/images/products/Vans/Vans_Women_Low-top_Trainers_Sneaker/2.png'),
+      require('../database/images/products/Vans/Vans_Women_Low-top_Trainers_Sneaker/3.png'),
+    ],
+  },
+  {
+    id: 12,
+    category: 'product',
+    productName: 'Vans_Women_Platform_Trainers_Sneaker',
+    productPrice: 109.99,
+    description:
+      'Inspired by Vans Classic Old Skool, the Ward is a low top, vulcanized lace up style with Vans signature side stripe, a round toe, padded collar for extra comfort, a smooth lining and a cushioning insole.',
+    isOff: false,
+    productImage: require('../database/images/products/Vans/Vans_Women_Platform_Trainers_Sneaker/1.png'),
+    isAvailable: true,
+    productImageList: [
+      require('../database/images/products/Vans/Vans_Women_Platform_Trainers_Sneaker/1.png'),
+      require('../database/images/products/Vans/Vans_Women_Platform_Trainers_Sneaker/2.png'),
+      require('../database/images/products/Vans/Vans_Women_Platform_Trainers_Sneaker/3.png'),
+    ],
+  },
+  //dây giày
+  {
+    id: 13,
+    category: 'accessory',
+    productName: 'BB_BROTHER_BROTHER_(7_Pairs_Colored_Oxford_Dress_Shoe_Laces_Shoe_Strings',
+    productPrice: 20,
+    description:
+      'To my knowledge there are several different color combinations for the same price. You should be able to choose what combination of colors you want.',
+    isOff: true,
+    productImage: require('../database/images/accessories/shoelace/BB_BROTHER_BROTHER_(7_Pairs_Colored_Oxford_Dress_Shoe_Laces_Shoe_Strings/1.png'),
+    isAvailable: true,
+    productImageList: [
+      require('../database/images/accessories/shoelace/BB_BROTHER_BROTHER_(7_Pairs_Colored_Oxford_Dress_Shoe_Laces_Shoe_Strings/1.png'),
+      require('../database/images/accessories/shoelace/BB_BROTHER_BROTHER_(7_Pairs_Colored_Oxford_Dress_Shoe_Laces_Shoe_Strings/2.png'),
+      require('../database/images/accessories/shoelace/BB_BROTHER_BROTHER_(7_Pairs_Colored_Oxford_Dress_Shoe_Laces_Shoe_Strings/3.png')
+    ],
+  },
+  {
+    id: 14,
+    category: 'accessory',
+    productName: 'IRONLACE_Unbreakable_Round_Bootlaces-Indestructible,Waterproof&Fir_ Resistant_Boot_&_Shoe_Laces',
+    productPrice: 17.99,
+    description:
+      'STRONGER THAN STEEL: Our durable, 100% advanced para-aramid fiber replacement lace is 10x stronger than steel; Amazing 1500-pound breaking strength/pair and a ⅛ inch (3.2mm) diameter means they will fit any boot and will never break when pulled tight',
+    isOff: false,
+    productImage: require('../database/images/accessories/shoelace/IRONLACE/1.png'),
+    isAvailable: true,
+    productImageList: [
+      require('../database/images/accessories/shoelace/IRONLACE/1.png'),
+      require('../database/images/accessories/shoelace/IRONLACE/2.png'),
+      require('../database/images/accessories/shoelace/IRONLACE/3.png')
+    ],
+  },
+  {
+    id: 15,
+    category: 'accessory',
+    productName: 'Miscly_Round_Boot Laces_[3 Pairs]_Heavy_Duty_and Durable_Shoelaces_for_Boots,Work_Boots&HikingShoes',
+    productPrice: 30,
+    description:
+      'Perfect for boots, work boots & hiking shoes, they also work great for other shoes when you just want extra strong and & sturdy laces',
+    isOff: false,
+    productImage: require('../database/images/accessories/shoelace/Miscly_Round_Boot/1.png'),
+    isAvailable: true,
+    productImageList: [
+      require('../database/images/accessories/shoelace/Miscly_Round_Boot/1.png'),
+      require('../database/images/accessories/shoelace/Miscly_Round_Boot/2.png'),
+      require('../database/images/accessories/shoelace/Miscly_Round_Boot/3.png')
+    ],
+  },
+  {
+    id: 16,
+    category: 'accessory',
+    productName: 'Waxed_Flat_Cotton_Shoelaces_7mm_Wide_For_Leather_Shoes_Sneaker_Boots',
+    productPrice: 7.50,
+    description:
+      '100% cotton by made , Waxed effect shoelaces, old style',
+    isOff: true,
+    productImage: require('../database/images/accessories/shoelace/Waxed_Flat_Cotton_Shoelaces_7mm_Wide_For_Leather_Shoes_Sneaker_Boots/1.png'),
+    isAvailable: true,
+    productImageList: [
+      require('../database/images/accessories/shoelace/Waxed_Flat_Cotton_Shoelaces_7mm_Wide_For_Leather_Shoes_Sneaker_Boots/1.png'),
+      require('../database/images/accessories/shoelace/Waxed_Flat_Cotton_Shoelaces_7mm_Wide_For_Leather_Shoes_Sneaker_Boots/2.png'),
+      require('../database/images/accessories/shoelace/Waxed_Flat_Cotton_Shoelaces_7mm_Wide_For_Leather_Shoes_Sneaker_Boots/3.png')
+    ],
+  },
+
+
+
+
+];
